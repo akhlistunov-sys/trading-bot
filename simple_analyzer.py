8383dcbe42a691a30ad4238351b3247c96e97afe
akhlistunov-sys <a.khlistunov@gmail.com>
2025-12-08 19:32:53 +0500

    Update simple_analyzer.py

--- simple_analyzer.py
+++ simple_analyzer.py
@@ -5,160 +5,166 @@ from typing import List, Dict
 logger = logging.getLogger(__name__)
 
 class SimpleAnalyzer:
-    """Простой анализатор новостей с быстрой предфильтрацией"""
+    """УЛУЧШЕННЫЙ анализатор с расширенным словарем тикеров"""
     
-    # Расширенный словарь тикеров
-        # Расширенный словарь тикеров (добавлены синонимы и общие термины)
+    # ==================== РАСШИРЕННЫЙ СЛОВАРЬ ТИКЕРОВ ====================
     TICKER_MAP = {
-        # Банки и фин. организации
-        'сбербанк': 'SBER', 'сбер': 'SBER', 'sber': 'SBER', 'сбербанка': 'SBER', 'сбербанке': 'SBER',
-        'втб': 'VTBR', 'втб банк': 'VTBR', 'втб банка': 'VTBR', 'vtb': 'VTBR', 'банк втб': 'VTBR',
-        'тинькофф': 'TCSG', 'tinkoff': 'TCSG', 'tcs': 'TCSG', 'тинькофф банк': 'TCSG', 'tinkoff bank': 'TCSG',
-        'альфа банк': 'ALFA', 'альфа-банк': 'ALFA', 'альфабанк': 'ALFA', 'альфа банка': 'ALFA',
-        'банк': 'SBER',  # Общий термин
-        'кредит': 'SBER',  # Общий финансовый термин
+        # === БАНКИ ===
+        'сбербанк': 'SBER', 'сбер': 'SBER', 'сбербанка': 'SBER', 'сбербанке': 'SBER',
+        'sberbank': 'SBER', 'sber': 'SBER', 'сбера': 'SBER',
+        'втб': 'VTBR', 'втб банк': 'VTBR', 'банк втб': 'VTBR', 'втб банка': 'VTBR',
+        'vtb': 'VTBR', 'vtb bank': 'VTBR',
+        'тинькофф': 'TCSG', 'тинькофф банк': 'TCSG', 'tinkoff': 'TCSG', 'tcs': 'TCSG',
+        'tinkoff bank': 'TCSG', 'тинькоффа': 'TCSG',
+        'альфа банк': 'ALFA', 'альфа-банк': 'ALFA', 'альфабанк': 'ALFA',
+        'альфа банка': 'ALFA', 'alfa bank': 'ALFA',
+        'открытие': 'FCIT', 'банк открытие': 'FCIT', 'открытия': 'FCIT',
+        'россельхозбанк': 'RUGR', 'рсхб': 'RUGR', 'сельхозбанк': 'RUGR',
+        'совкомбанк': 'SVCB', 'совком': 'SVCB',
         
-        # Нефтегазовый сектор
-        'газпром': 'GAZP', 'газ': 'GAZP', 'gazprom': 'GAZP', 'газпрома': 'GAZP', 'газпроме': 'GAZP',
-        'нефть': 'GAZP', 'нефти': 'GAZP', 'нефтяной': 'GAZP',  # Общие термины
-        'лукойл': 'LKOH', 'лук': 'LKOH', 'lukoil': 'LKOH', 'лукойла': 'LKOH',
-        'роснефть': 'ROSN', 'роспефть': 'ROSN', 'rosneft': 'ROSN', 'роснефти': 'ROSN',
-        'нефтяная компания': 'ROSN',  # Общий термин
+        # === НЕФТЬ И ГАЗ ===
+        'газпром': 'GAZP', 'газ': 'GAZP', 'газпрома': 'GAZP', 'газпроме': 'GAZP',
+        'gazprom': 'GAZP', 'газпрому': 'GAZP',
+        'лукойл': 'LKOH', 'лук': 'LKOH', 'лукойла': 'LKOH', 'lukoil': 'LKOH',
+        'роснефть': 'ROSN', 'роспефть': 'ROSN', 'роснефти': 'ROSN',
+        'rosneft': 'ROSN', 'нефтяная компания': 'ROSN',
         'новатэк': 'NVTK', 'novatek': 'NVTK', 'новатека': 'NVTK',
         'татнефть': 'TATN', 'tatneft': 'TATN', 'татнефти': 'TATN',
-        'башнефть': 'BANE', 'bashneft': 'BANE',
+        'башнефть': 'BANE', 'bashneft': 'BANE', 'башнефти': 'BANE',
         'сургутнефтегаз': 'SNGS', 'сургут': 'SNGS', 'сургутнефтегаза': 'SNGS',
-        'газовая компания': 'GAZP',  # Общий термин
+        'surgutneftegas': 'SNGS',
         
-        # Металлургия и добыча
-        'норильский никель': 'GMKN', 'норникель': 'GMKN', 'nornickel': 'GMKN', 'норникеля': 'GMKN',
-        'никель': 'GMKN',  # Общий термин
-        'алроса': 'ALRS', 'алроса': 'ALRS', 'alrosa': 'ALRS', 'алросы': 'ALRS',
-        'бриллиант': 'ALRS', 'алмаз': 'ALRS',  # Общие термины
-        'поллиметалл': 'POLY', 'polymetal': 'POLY', 'полиметалл': 'POLY',
-        'золото': 'POLY', 'золотодобыча': 'POLY',  # Общие термины
+        # === МЕТАЛЛУРГИЯ И ГОРН. ДОБЫЧА ===
+        'норильский никель': 'GMKN', 'норникель': 'GMKN', 'норникеля': 'GMKN',
+        'nornickel': 'GMKN', 'никель': 'GMKN',
+        'алроса': 'ALRS', 'алросы': 'ALRS', 'алросу': 'ALRS', 'alrosa': 'ALRS',
+        'полиметалл': 'POLY', 'polymetal': 'POLY', 'полиметалла': 'POLY',
         'северсталь': 'CHMF', 'severstal': 'CHMF', 'северстали': 'CHMF',
-        'сталь': 'CHMF', 'металлург': 'CHMF',  # Общие термины
-        'нлмк': 'NLMK', 'nlmk': 'NLMK',
+        'нлмк': 'NLMK', 'nlmk': 'NLMK', 'нлмка': 'NLMK',
         'мечел': 'MTLR', 'mechel': 'MTLR', 'мечела': 'MTLR',
+        'ммк': 'MAGN', 'магнитогорск': 'MAGN', 'магнитка': 'MAGN',
+        'распадская': 'RASP', 'распадской': 'RASP',
         
-        # Ритейл и потребительский сектор
-        'магнит': 'MGNT', 'магнит': 'MGNT', 'magnit': 'MGNT', 'магнита': 'MGNT',
-        'супермаркет': 'MGNT', 'ритейл': 'MGNT',  # Общие термины
-        'х5 ритейл': 'FIVE', 'x5': 'FIVE', 'x5 retail': 'FIVE', 'х5': 'FIVE', 'х5 ритейла': 'FIVE',
-        'пятерочка': 'FIVE', 'перекресток': 'FIVE',  # Магазины сети X5
-        'лэнта': 'LNTA', 'lenta': 'LNTA', 'лента': 'LNTA',
+        # === РИТЕЙЛ И ПОТРЕБ. СЕКТОР ===
+        'магнит': 'MGNT', 'магнита': 'MGNT', 'magnit': 'MGNT',
+        'х5 ритейл': 'FIVE', 'x5': 'FIVE', 'x5 ритейла': 'FIVE', 'x5 retail': 'FIVE',
+        'пятерочка': 'FIVE', 'перекресток': 'FIVE', 'карусель': 'FIVE',
+        'лэнта': 'LNTA', 'lenta': 'LNTA', 'лента': 'LNTA', 'ленты': 'LNTA',
         'озон': 'OZON', 'ozon': 'OZON', 'озона': 'OZON',
         'яндекс': 'YNDX', 'yandex': 'YNDX', 'яндекса': 'YNDX',
-        'м.видео': 'MVID', 'мвидео': 'MVID', 'м.видео': 'MVID',
-        'эльдорадо': 'MVID',  # Входит в ту же группу
+        'м.видео': 'MVID', 'мвидео': 'MVID', 'мвидео': 'MVID', 'эльдорадо': 'MVID',
+        'wildberries': 'WB', 'вайлдбериз': 'WB', 'wildberry': 'WB',
+        'детский мир': 'DSKY', 'детского мира': 'DSKY',
+        'черкизово': 'GCHE', 'черкизова': 'GCHE',
         
-        # Телеком и технологии
+        # === ТЕЛЕКОМ И ТЕХНОЛОГИИ ===
         'мтс': 'MTSS', 'mts': 'MTSS', 'мтс': 'MTSS',
         'мосбиржа': 'MOEX', 'moex': 'MOEX', 'мосбиржи': 'MOEX',
-        'биржа': 'MOEX',  # Общий термин
         'ростелеком': 'RTKM', 'rostelecom': 'RTKM', 'ростелекома': 'RTKM',
-        'интернет': 'RTKM', 'телеком': 'RTKM',  # Общие термины
+        'билайн': 'BEEL', 'beeline': 'BEEL', 'вымпелком': 'BEEL',
+        'мегафон': 'MFON', 'megafon': 'MFON', 'мегафона': 'MFON',
         
-        # Химия и удобрения
+        # === ХИМИЯ И УДОБРЕНИЯ ===
         'фосагро': 'PHOR', 'phosagro': 'PHOR', 'фосагро': 'PHOR',
         'акрон': 'AKRN', 'akron': 'AKRN', 'акрона': 'AKRN',
         'куйбышевазот': 'KAZT', 'kuybyshevazot': 'KAZT',
-        'удобрени': 'PHOR',  # Общий термин
+        'уралкалий': 'URKA', 'uralkali': 'URKA', 'уралкалия': 'URKA',
         
-        # Автомобилестроение
-        'автоваз': 'AVAZ', 'lada': 'AVAZ', 'ваз': 'AVAZ', 'лада': 'AVAZ',
-        'автомобил': 'AVAZ',  # Общий термин
-        
-        # Энергетика
+        # === ЭНЕРГЕТИКА ===
         'россети': 'RSTI', 'rosseti': 'RSTI', 'россетей': 'RSTI',
         'интер рао': 'IRAO', 'inter rao': 'IRAO', 'интер рао': 'IRAO',
-        'фск': 'FEES', 'fsk': 'FEES',
-        'электроэнерг': 'FEES',  # Общий термин
+        'фск': 'FEES', 'fsk': 'FEES', 'фск еэс': 'FEES',
+        'русгидро': 'HYDR', 'rushydro': 'HYDR', 'русгидро': 'HYDR',
+        'эн+': 'ENPL', 'енплюс': 'ENPL', 'en+': 'ENPL',
         
-        # Общие финансовые термины (добавлены для увеличения coverage)
+        # === ТРАНСПОРТ И ЛОГИСТИКА ===
+        'аэрофлот': 'AFLT', 'aeroflot': 'AFLT', 'аэрофлота': 'AFLT',
+        'новатэк': 'NVTK',  # уже есть, но для логистики
+        'globaltrans': 'GLTR', 'глобалтранс': 'GLTR',
+        
+        # === ФИНАНСОВЫЕ ТЕРМИНЫ (ОБЩИЕ) ===
         'акция': 'SBER', 'акций': 'SBER', 'акциями': 'SBER',
         'дивиденд': 'SBER', 'дивиденды': 'SBER', 'дивидендов': 'SBER',
         'отчетность': 'SBER', 'квартал': 'SBER', 'прибыль': 'SBER',
-        'рынок': 'MOEX', 'фондовый рынок': 'MOEX',
-        'инвестор': 'SBER', 'инвестиции': 'SBER',
-        'котировки': 'MOEX', 'торги': 'MOEX',
+        'выручка': 'SBER', 'убыток': 'SBER', 'ebitda': 'SBER',
+        'рынок': 'MOEX', 'фондовый': 'MOEX', 'бирж': 'MOEX',
+        'инвестор': 'SBER', 'инвестиции': 'SBER', 'инвестицион': 'SBER',
+        'трейдер': 'MOEX', 'трейдинг': 'MOEX', 'торговля': 'MOEX',
+        'портфель': 'SBER', 'котировки': 'MOEX', 'листинг': 'MOEX',
+        'эмиссия': 'SBER', 'облигации': 'SBER', 'купон': 'SBER',
+        'дивидендная': 'SBER', 'дивидендную': 'SBER',
+        
+        # === РЕГУЛЯТОРЫ И ПРАВИТЕЛЬСТВО ===
+        'цб': 'SBER', 'центральный банк': 'SBER', 'банк россии': 'SBER',
+        'минфин': 'SBER', 'министерство финансов': 'SBER',
+        'правительство': 'SBER', 'правительства': 'SBER',
+        'санкции': 'SBER', 'санкцион': 'SBER', 'ограничен': 'SBER',
+        
+        # === МЕДИЦИНА И ФАРМА ===
+        'фармацевт': 'PHOR', 'лекарств': 'PHOR', 'вакцин': 'PHOR',
+        'биотехнологи': 'PHOR', 'медицинск': 'PHOR', 'здравоохран': 'PHOR',
+        'аптек': 'MGNT', 'лечени': 'MGNT', 'здоровь': 'MGNT',
+        'диагностик': 'MGNT', 'больниц': 'MGNT', 'поликлиник': 'MGNT',
+        
+        # === СТРОИТЕЛЬСТВО И НЕДВИЖИМОСТЬ ===
+        'пик': 'PIKK', 'pikk': 'PIKK', 'пика': 'PIKK',
+        'лср': 'LSRG', 'lsr': 'LSRG', 'лср групп': 'LSRG',
+        'эталон': 'ETLN', 'etalon': 'ETLN', 'эталона': 'ETLN',
+        'самолет': 'SMLT', 'samolyot': 'SMLT', 'самолета': 'SMLT',
+        
+        # === АВТОМОБИЛИ ===
+        'автоваз': 'AVAZ', 'lada': 'AVAZ', 'ваз': 'AVAZ', 'лада': 'AVAZ',
+        'газ': 'GAZ', 'газ группа': 'GAZ', 'газа': 'GAZ',
+        'соллерс': 'SVAV', 'sollers': 'SVAV', 'соллерса': 'SVAV',
     }
     
-    # Ключевые слова для БЫСТРОЙ предфильтрации - новость должна содержать хотя бы одно из этих слов
+    # Ключевые слова для быстрой предфильтрации (РАСШИРЕННЫЕ)
     QUICK_FILTER_KEYWORDS = [
-        # Финансовые триггеры
-        'дивиденд', 'отчет', 'прибыль', 'выручка', 'квартал', 'годовой',
-        'продаж', 'покупк', 'сделка', 'актив', 'акции', 'облигац',
-        # Действия
-        'рост', 'падение', 'увеличил', 'снизил', 'повысил', 'понизил',
-        'рекомендует', 'советует', 'предлагает', 'ожидает',
-        # События
-        'слияние', 'поглощение', 'банкротство', 'санкц', 'запрет',
-        'лиценз', 'регулятор', 'правительств', 'закон',
-        # Общие рыночные
-        'рынок', 'бирж', 'инвест', 'трейд', 'торговл', 'котировк'
+        'акци', 'дивиденд', 'отчет', 'прибыль', 'выручка', 'убыток',
+        'квартал', 'годовой', 'финансов', 'результат', 'ebitda',
+        'покупк', 'продаж', 'сделка', 'слияни', 'поглощен',
+        'рост', 'падение', 'увелич', 'снижен', 'повышен', 'понижен',
+        'рекоменду', 'советует', 'предлагает', 'ожидает', 'прогноз',
+        'санкц', 'запрет', 'лиценз', 'регулятор', 'правительств',
+        'закон', 'постановлен', 'распоряжен',
+        'война', 'конфликт', 'политика', 'международн', 'санкци',
+        'рынок', 'бирж', 'инвест', 'трейд', 'торговл', 'котировк',
+        'рубл', 'доллар', 'евро', 'нефт', 'газ', 'золот', 'серебр',
+        'эмисси', 'облигац', 'купон', 'погашен', 'выпуск',
+        'дивидендн', 'выплат', 'доходност',
+        'банк', 'кредит', 'ипотек', 'вклад', 'депозит',
+        'компани', 'корпорац', 'холдинг', 'групп', 'предприят',
+        'совет директоров', 'собрание', 'голосование',
+        'аналитик', 'эксперт', 'оптимизм', 'пессимизм'
     ]
-    
-    # Ключевые слова для анализа тональности
-    POSITIVE_WORDS = [
-        'рост', 'прибыль', 'увеличил', 'повысил', 'дивиденды', 'рекомендует', 'успех',
-        'рекорд', 'прогресс', 'улучшение', 'позитивный', 'оптимизм', 'сильный', 'стабильный',
-        'выше ожиданий', 'превысил', 'улучшил', 'восстановление', 'процветание'
-    ]
-    
-    NEGATIVE_WORDS = [
-        'падение', 'убыток', 'снизил', 'сократил', 'проблемы', 'риски', 'сложности',
-        'кризис', 'снижение', 'негативный', 'ухудшение', 'слабый', 'нестабильный',
-        'ниже ожиданий', 'упал', 'провал', 'увольнения', 'банкротство', 'санкции'
-    ]
-    
-    # Ключевые слова для типов событий
-    EVENT_KEYWORDS = {
-        'earnings_report': ['отчет', 'прибыль', 'выручка', 'квартал', 'финансовые результаты', 'ebitda'],
-        'dividend': ['дивиденд', 'дивиденды', 'выплата', 'доходность'],
-        'merger_acquisition': ['слияние', 'поглощение', 'покупка', 'продажа', 'сделка', 'активы'],
-        'regulatory': ['регулятор', 'закон', 'правительство', 'санкции', 'запрет', 'лицензия'],
-        'geopolitical': ['война', 'конфликт', 'политика', 'международный', 'санкции'],
-        'corporate_action': ['совет директоров', 'собрание', 'голосование', 'эмиссия']
-    }
     
     @staticmethod
     def should_analyze_further(news_item: Dict) -> bool:
-        """Быстрая предфильтрация: стоит ли анализировать новость глубже?"""
+        """Быстрая предфильтрация финансовых новостей"""
         title = news_item.get('title', '').lower()
         content = news_item.get('content', '').lower() or news_item.get('description', '').lower()
-        text = title + ' ' + content[:200]  # Смотрим только начало для скорости
+        text = title + ' ' + content[:300]
         
         # 1. Проверяем наличие финансовых триггеров
         has_keywords = any(keyword in text for keyword in SimpleAnalyzer.QUICK_FILTER_KEYWORDS)
         if not has_keywords:
-            logger.debug(f"⏩ Пропускаем новость (нет ключевых слов): {title[:50]}...")
             return False
         
         # 2. Быстрый поиск тикеров
         has_tickers = any(keyword in text for keyword in SimpleAnalyzer.TICKER_MAP.keys())
         if not has_tickers:
-            logger.debug(f"⏩ Пропускаем новость (нет тикеров): {title[:50]}...")
             return False
-        
-        # 3. Проверяем источник (опционально)
-        source = news_item.get('source', '').lower()
-        if 'блог' in source or 'форум' in source:
-            logger.debug(f"⚠️ Новость из ненадежного источника: {source}")
-            return False  # или можно возвращать True, но с пониженным приоритетом
         
         return True
     
     @staticmethod
     def analyze_news(news_item: Dict) -> Dict:
-        """Полный анализ новости после предфильтрации"""
-        
+        """Полный анализ новости"""
         title = news_item.get('title', '').lower()
         content = news_item.get('content', '').lower() or news_item.get('description', '').lower()
         text = title + ' ' + content
         
-        # Извлекаем тикеры
+        # Извлекаем тикеры (РАСШИРЕННЫЙ ПОИСК)
         tickers = []
         for keyword, ticker in SimpleAnalyzer.TICKER_MAP.items():
             if keyword in text:
@@ -167,46 +173,52 @@ class SimpleAnalyzer:
         # Удаляем дубликаты
         tickers = list(set(tickers))
         
-        # Анализ тональности
-        positive_count = sum(1 for word in SimpleAnalyzer.POSITIVE_WORDS if word in text)
-        negative_count = sum(1 for word in SimpleAnalyzer.NEGATIVE_WORDS if word in text)
+        # Более агрессивный анализ тональности
+        positive_words = ['рост', 'прибыль', 'увелич', 'повыш', 'рекорд', 'успех', 
+                         'прогресс', 'улучшен', 'позитив', 'оптимизм', 'сильн', 'стабильн']
+        negative_words = ['падение', 'убыток', 'снижен', 'сокращен', 'проблем', 'риск',
+                         'сложност', 'кризис', 'негатив', 'ухудшен', 'слаб', 'нестабильн']
+        
+        positive_count = sum(1 for word in positive_words if word in text)
+        negative_count = sum(1 for word in negative_words if word in text)
         
         if positive_count > negative_count:
             sentiment = 'positive'
-            impact_score = min(8, 4 + positive_count)
+            impact_score = min(8, 5 + positive_count)
         elif negative_count > positive_count:
             sentiment = 'negative'
-            impact_score = min(8, 4 + negative_count)
+            impact_score = min(8, 5 + negative_count)
         else:
             sentiment = 'neutral'
-            impact_score = 4
+            impact_score = 5
         
         # Определяем тип события
         event_type = 'market_update'
-        max_keyword_count = 0
+        if 'дивиденд' in text:
+            event_type = 'dividend'
+        elif 'отчет' in text or 'квартал' in text:
+            event_type = 'earnings_report'
+        elif 'слияни' in text or 'поглощен' in text:
+            event_type = 'merger_acquisition'
+        elif 'санкц' in text or 'регулятор' in text:
+            event_type = 'regulatory'
         
-        for event_type_name, keywords in SimpleAnalyzer.EVENT_KEYWORDS.items():
-            keyword_count = sum(1 for word in keywords if word in text)
-            if keyword_count > max_keyword_count:
-                max_keyword_count = keyword_count
-                event_type = event_type_name
-        
-        # Релевантность
-        relevance_score = 60 if tickers else 30
+        # Релевантность и confidence
+        relevance_score = 70 if tickers else 40
         if tickers and (positive_count > 0 or negative_count > 0):
-            relevance_score = 75
+            relevance_score = 85
         
-        # Confidence (уверенность) зависит от количества найденных триггеров
-        confidence = 0.3 + min(0.4, (positive_count + negative_count) * 0.05)
+        confidence = 0.4 + min(0.4, (positive_count + negative_count) * 0.05)
         if tickers:
             confidence += 0.3
+        confidence = min(0.9, confidence)  # Ограничиваем
         
-        # Создаем summary
+        # Summary
         if tickers:
             tickers_str = ', '.join(tickers[:3])
-            summary = f"Локальный анализ: найдены {len(tickers)} тикеров ({tickers_str}), тональность: {sentiment}, уверенность: {confidence:.1f}"
+            summary = f"Найдены тикеры: {tickers_str}, тональность: {sentiment}"
         else:
-            summary = f"Локальный анализ: тикеры не найдены, тональность: {sentiment}"
+            summary = f"Тикеры не найдены, тональность: {sentiment}"
         
         return {
             'tickers': tickers,
